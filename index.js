e507389c2c25e854cf981e9a9135f11cb2d4e2e6
Vitamax Oduol <85936735+vitamaxoduol@users.noreply.github.com>
2023-06-18 15:49:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Code your solution here
+
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers, name) {
+    
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+
+}
+// let NamesOfDrivers = findMatching(drivers, name);
+
+console.log(findMatching(drivers, 'Bobby'));
+console.log(findMatching(drivers, 'Sammy'));
+console.log(findMatching(drivers, 'Susan'));
+
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+console.log(fuzzyMatch(drivers, 'letters')); //Output
+
+
+function matchName(drivers, name) {
+    const matchingDrivers = drivers.filter(driver => driver.name === name);
+    return matchingDrivers;
+}
+const newDrivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh'
+    },
+    {
+      name: 'Sammy',
+      hometown: 'New York'
+    },
+    {
+      name: 'Sally',
+      hometown: 'Cleveland'
+    },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles'
+    },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay'
+    }
+  ];
+  
+console.log(matchName(newDrivers, 'Bobby'));
